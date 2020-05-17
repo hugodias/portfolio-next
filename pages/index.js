ad8b80fef673b71f2df4bda8c22795c5341bfcd3
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2020-05-17 09:43:29 -0300

    fix layoyut component import

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Layout from "../components/layout";
+import Layout from "components/layout";
 import Hero from "components/hero";
 import Intro from "components/intro";
 import Customers from "components/customers";
